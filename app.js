38a7cd48bd416d0acd9e9da9f113ffba63a7f248
Vinit-Technman <vinit.c@technmanconsulting.com>
2024-02-07 13:35:45 -0500

    Added Pm2 File

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express=require("express");
 const app=express();
 
 
+app.get("/",(req,res)=>{
+    res.send("Hello this is v1.1");
+})
+
 app.listen(3000,()=>{
     console.log("Server Starts on port ",3000);
 });
